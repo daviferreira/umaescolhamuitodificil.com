12f6c5e9dd35b431c8a81d9ad6fd66e81820a7b9
Davi Ferreira <contato@daviferreira.com>
2020-03-25 13:17:09 +0100

    Add video

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,16 @@
 import React from "react"
-import { Link } from "gatsby"
+import BozoVideo from "../assets/video.mp4"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
-import styles from "./styles.module.css"
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div className={styles.cover}>
-      <div className={styles.image}>
-        <Image />
-      </div>
-      <div className={styles.content}>
-        <div className={styles.titleContainer}>
-          <h1 className={styles.title}>Uma escolha muito difícil</h1>
-        </div>
-        <p className={styles.intro}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-          ullamcorper mi est, pretium auctor sapien imperdiet nec. Maecenas nec
-          turpis non odio fermentum varius eu vel sapien. Donec at cursus odio.
-        </p>
-        <div className={styles.actions}>
-          <Link to="/page-2" className={styles.button} role="button">
-            Lorem ipsum dolor
-          </Link>
-        </div>
-      </div>
-    </div>
-  </Layout>
+export default () => (
+  <div
+    style={{
+      background: 'black',
+      height: "100vh",
+      width: "100vw",
+    }}
+  >
+    <video autoPlay controls height="100%" width="100%">
+      <source src={BozoVideo} type="video/mp4" />
+    </video>
+  </div>
 )
-
-export default IndexPage
