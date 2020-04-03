feeff06e7b7aee2713163f3285143a1f49db0134
Davi Ferreira <contato@daviferreira.com>
2020-04-03 13:31:37 +0200

    WIP

--- src/components/Mosaic/index.js
+++ src/components/Mosaic/index.js
@@ -4,6 +4,17 @@ import Image from "./image"
 
 import styles from "./styles.module.css"
 
+const items = [{
+  id: 1,
+  title: 'Milícia'
+}, {
+  id: 1,
+  title: 'Presidência'
+}, {
+  id: 1,
+  title: 'COVID-19'
+}];
+
 const Mosaic = () => {
   useEffect(() => {
     document.body.classList.add("internal")
@@ -11,46 +22,52 @@ const Mosaic = () => {
     return () => {
       document.body.classList.remove("internal")
     }
-  });
+  })
 
   return (
     <div className={styles.root}>
-      <div className={styles.row}>
-        <div className={styles.item}>
-          <div className={styles.content}>
-            <Image index={1} />
-            <h2 className={styles.title}>
-              <span>Early days</span>
-            </h2>
-          </div>
-        </div>
-        <div className={styles.item}>
-          <div className={styles.content}>
-            <Image index={2} />
-            <h2 className={styles.title}>
-              <span>Planalto</span>
-            </h2>
-          </div>
-        </div>
-        <div className={styles.item}>
-          <div className={styles.content}>
-            <Image index={3} />
-            <h2 className={styles.title}>
-              <span>Campanha</span>
-            </h2>
-          </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <Image index={1} />
+          <h2 className={styles.title}>
+            <span>Early days</span>
+          </h2>
         </div>
       </div>
-      <div className={styles.row}>
-        <div className={styles.item}>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <Image index={2} />
+          <h2 className={styles.title}>
+            <span>Planalto</span>
+          </h2>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <Image index={3} />
+          <h2 className={styles.title}>
+            <span>Campanha</span>
+          </h2>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <Image index={4} />
+          <h2 className={styles.title}>
+            <span>Família</span>
+          </h2>
+        </div>
+      </div>
+      {items.map(({ id, title }) => (
+        <div className={styles.item} key={id}>
           <div className={styles.content}>
             <Image index={4} />
             <h2 className={styles.title}>
-              <span>Família</span>
+              <span>{title}</span>
             </h2>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   )
 }
