d6e95d3234afc15f20cebf35ecc07fef64518428
Davi Ferreira <contato@daviferreira.com>
2020-05-02 21:22:15 +0200

    Fix title

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Uma escolha muito difícil
+# Uma escolha muito difícil?
 
 Desenvolvido por [Davi Ferreira](http://twitter.com/davitferreira/) com base no gráfico criado por [Julio Ponce](https://twitter.com/jasonptodd).
 
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Uma escolha muito difícil`,
+    title: `Uma escolha muito difícil?`,
     description: `Gráfico interativo mostrando a relação das frases de Bolsonaro durante a pandemia do novo coronavírus com o número de casos e mortes.`,
     author: `@davitferreira`
   },
